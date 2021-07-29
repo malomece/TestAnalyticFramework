20c7dd92eda5532f828a06f0375f04f539673cb8
kiril <malomece@hotmail.com>
2021-07-29 12:29:12 +0200

    Start

---
+++ TestAnalyticFramework/TestAnalyticFramework/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TestAnalyticFramework
+//
+//  Created by Kiril Kiroski on 7/29/21.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func doSomething() -> String{
+        return "You make something!!!"
+    }
+}
---
+++ TestAnalyticFramework/TestAnalyticFramework/TestAnalyticFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestAnalyticFramework.h
+//  TestAnalyticFramework
+//
+//  Created by Kiril Kiroski on 7/29/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestAnalyticFramework.
+FOUNDATION_EXPORT double TestAnalyticFrameworkVersionNumber;
+
+//! Project version string for TestAnalyticFramework.
+FOUNDATION_EXPORT const unsigned char TestAnalyticFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestAnalyticFramework/PublicHeader.h>
+
+
